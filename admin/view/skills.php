06da427e95103a195f32e944f6c50218ab890a56
Md. Hossni Mubarak <mhmrajib@gmail.com>
2025-12-13 19:39:06 +0600

    working on skill settings

--- admin/view/skills.php
+++ admin/view/skills.php
@@ -3,15 +3,19 @@ $hmrmShowSkillsMessage = false;
 //delete_option('hmrm_skills_settings');
 
 if (isset($_POST['updateSettings'])) {
+    
     if (!isset($_POST['hmrm_update_skills_setting'])) die("Something wrong!");
     if (!wp_verify_nonce($_POST['hmrm_update_skills_setting'], 'hmrm-update-skills-setting')) die("Something wrong!");
+    
     for ($i = 0; $i < count($_POST['hmrm_skill_name']); $i++) {
+        
         $hmrmSkillArr[$i] = array(
             'hmrm_skill_name'   => sanitize_text_field($_POST['hmrm_skill_name'][$i]) ? sanitize_text_field($_POST['hmrm_skill_name'][$i]) : 'Sample Skill',
             'hmrm_skill_percentage'   => sanitize_text_field($_POST['hmrm_skill_percentage'][$i]) ? sanitize_text_field($_POST['hmrm_skill_percentage'][$i]) : '100',
             'hmrm_skill_bg_color'   => sanitize_text_field($_POST['hmrm_skill_bg_color'][$i]) ? sanitize_text_field($_POST['hmrm_skill_bg_color'][$i]) : '#009900',
         );
     }
+
     $hmrmShowSkillsMessage = update_option('hmrm_skills_settings', $hmrmSkillArr);
 }
 $hmrmSkillsSettings = get_option('hmrm_skills_settings');
@@ -22,13 +26,13 @@ $hmrmSkillsSettings = get_option('hmrm_skills_settings');
     
     <div class="hmcs-header-bar">
         <div class="hmcs-header-left">
-            <h3 class="hmcs-header-title"><i class="fa fa-cogs" aria-hidden="true"></i>&nbsp;<?php _e('Skills Settings', HMRM_TXT_DOMAIN); ?></h3>
+            <h3 class="hmcs-header-title"><i class="fa fa-cogs" aria-hidden="true"></i>&nbsp;<?php _e('Skills Settings', 'hm-resume-manager'); ?></h3>
         </div>
     </div>
 
     <?php 
     if ( $hmrmShowSkillsMessage ) {
-        $this->hmrm_display_notification('success', __('Your information updated successfully', HMRM_TXT_DOMAIN) );
+        $this->hmrm_display_notification('success', __('Your information updated successfully', 'hm-resume-manager') );
     }
     ?>
 
@@ -37,8 +41,7 @@ $hmrmSkillsSettings = get_option('hmrm_skills_settings');
         <div class="hmrm_personal_wrap hmrm_personal_help" style="width: 75%; float: left;">
 
             <form name="wpre-table" role="form" class="form-horizontal" method="post" action="" id="hmrm-settings-form">
-                <input type="hidden" name="hmrm_update_skills_setting"
-                    value="<?php printf('%s', wp_create_nonce('hmrm-update-skills-setting')); ?>" />
+                <input type="hidden" name="hmrm_update_skills_setting" value="<?php printf('%s', wp_create_nonce('hmrm-update-skills-setting')); ?>" />
                 <table class="hmrm-skills-table">
                     <tr>
                         <td colspan="2">
@@ -107,7 +110,7 @@ $hmrmSkillsSettings = get_option('hmrm_skills_settings');
                     </tr>
                 </table>
                 <p class="submit"><button id="updateSettings" name="updateSettings"
-                        class="hmcs-btn"><?php esc_html_e('Update Skills', HMRM_TXT_DOMAIN); ?></button></p>
+                        class="hmcs-btn"><?php esc_html_e('Update Skills', 'hm-resume-manager'); ?></button></p>
             </form>
         
         </div>
